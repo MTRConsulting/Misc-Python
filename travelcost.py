dcef9509c5f518bae533be83fe2fc8245a0b2bd4
mtryan <michael.ryan@28msec.com>
2017-02-23 16:42:49 -0600

    Corrected trip_cost return added call to trip_cost

--- travelcost.py
+++ travelcost.py
@@ -16,5 +16,8 @@ def rental_car_cost(days):
         return (days * 40) - 20
     else: 
         return (days * 40)
+def trip_cost(city, days):
+    return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city)
+        
     
-    
+trip_cost('Tampa', 3)
